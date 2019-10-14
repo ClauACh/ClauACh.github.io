8cbd6638938273f1c95e08cdca382809f72d229c
Claudia Aparicio <clau.ach07@gmail.com>
2019-10-14 01:25:11 -0400

    intro animation

---
+++ JS/anim.js
@@ -0,0 +1,14 @@
+var string = "Biologist and Front-end Web develloper"; /* type your text here */
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("intro").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',100); /* change 70 for speed */
+
+}
+frameLooper();
